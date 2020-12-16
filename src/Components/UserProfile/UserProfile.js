02dd90f65dea0b5a0939e04548b01f6e7e401083
Alan Scott <alan@Alans-MBP.home>
2020-12-16 12:54:37 -0800

    connectivity established between UserProfile React component and Spring server

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,10 +1,27 @@
 import './UserProfile.css';
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function UserProfile() {
+
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    async function fetchProfile() {
+      const response = await fetch('http://localhost:8080/users/1');
+      const jsonResponse = await response.json();
+
+      setProfile(jsonResponse);
+    }
+    fetchProfile();
+  }, []);
+
   return (
     <>
-      <h1>this is the user profile</h1>
+      <h3>username: {profile.username}</h3>
+      <h3>age: {profile.age}</h3>
+      <h3>location: {profile.location}</h3>
+      
       <Link to="/"><button>Login</button></Link>
       <br /><br />
       <Link to="/conversations"><button>Chat</button></Link> 
