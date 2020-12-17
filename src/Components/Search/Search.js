5dd569cdd8fc62e7a457c0f077890f040bc8dbc3
jrconsole <jrconsole@gmail.com>
2020-12-16 17:08:29 -0800

    Add search filter by location

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -9,11 +9,15 @@ function Search() {
   const [locations, setLocations] = useState([]);
   const [hobbies, setHobbies] = useState([]);
 
+  const [currUsers, setCurrUsers] = useState([]);
+  const [activeFilters, setActiveFilters] = useState({ location: null, hobbies: [] });
+
   const getUsers = async () => {
     const response = await fetch('http://localhost:8080/users'); //fetch users from server
     const returnedUsers = await response.json();
 
     setUsers(returnedUsers);
+    setCurrUsers(returnedUsers);
   }
 
   const getLocations = async () => {
@@ -36,8 +40,39 @@ function Search() {
     getHobbies();
   }, []);
 
+  const filterUsers = () => {
+    const filteredUsers = users.filter(user => {
+      return user.location_id === Number(activeFilters.location);
+    });
+
+    setCurrUsers(filteredUsers);
+  }
+
+  const filterByLocation = (selectedLocation) => {
+    if (Number(selectedLocation) === 0) {
+      setCurrUsers(users);
+    } else {
+      const state = activeFilters;
+      state.location = selectedLocation;
+      setActiveFilters(state);
+
+      filterUsers();
+    }
+  }
+
+  const renderLocationSelect = () => {
+    return (
+      <select name="locations" id="locations"  onChange={(e) => filterByLocation(e.target.value)}>
+        <option value={0}>All</option>
+        {locations.map(location => {
+          return <option value={location.location_id}>{location.name}</option>
+        })}
+      </select>
+    );
+  }
+
   const renderUsers = () => {
-    return users.map(user => {
+    return currUsers.map(user => {
       return (
         <>
           <Link to="/user" key={user.user_id}>{user.username}</Link>
@@ -50,6 +85,8 @@ function Search() {
   return (
     <>
       <h1>this is the Search page</h1>
+      {renderLocationSelect()}
+      <br/><br />
       {renderUsers()}
       <br /><br />
       <Link to="/user"><button>User Profile</button></Link>
