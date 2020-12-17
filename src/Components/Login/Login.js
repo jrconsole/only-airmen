68d52847ec9474bda9759e33ab35b328c657fccb
agadient <agadient@mit.edu>
2020-12-17 12:56:24 -0700

    add login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,49 @@
 import './Login.css';
 import { Link } from "react-router-dom";
-
+import React, { useState } from 'react';
+var PORT = 8080
 function NewComp() {
-  return (
-    <>
-      <h1>this is the login page</h1>
-      <Link to="/user"><button>User Login</button></Link>
-    </>
-  );
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const postCreds = async (e) => {
+        e.preventDefault();
+        const studentResponse = await fetch(`http://localhost:${PORT}/auth/login`, 
+                                            {method: 'POST',  body: JSON.stringify({username:username,password:password}),
+                                            headers: {
+                                            'Content-Type': 'application/json'
+                                            },
+                                            credentials: 'include'     
+                                            })
+    }
+
+    const logout = async(e) => {
+    
+
+    }
+
+    return (
+        <>
+        <h1>Login</h1>
+              <div>
+                <form onSubmit={postCreds}>
+                    <label>
+                    Username:
+                    <input type="text" name="username" onChange={(e)=>setUsername(e.target.value)} />
+                    </label>
+                    <br/>
+                    <label>
+                    Password:
+                    <input type="text" name="password" onChange={(e)=>setPassword(e.target.value)} />
+                     </label>
+                    <input type="submit" value="Submit"/>
+                    </form>
+              </div>
+        </>
+    );
 }
+
+
+
+
 
 export default NewComp;
